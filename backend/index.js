aaaf47ff5781953d5b130afc95855ada05e89a79
nico-a-fernandez <117313555+nico-a-fernandez@users.noreply.github.com>
2023-11-23 18:06:55 -0300

    creat a .gitignore and a .env
    also starting connecting to the chat engine rest API

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
@@ -7,6 +8,15 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
 	const { username } = req.body;
+
+	try {
+		const r = await axios.put("https://api.chatengine.io/users/", {
+			username: username,
+			secret: username,
+			first_name: username,
+		});
+	} catch (error) {}
+
 	return res.json({ username: username, secret: "sha256..." });
 });
 
