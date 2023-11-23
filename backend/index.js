c03d848ffe681a89bed92fe415bac2e3128d4b8f
nico-a-fernandez <117313555+nico-a-fernandez@users.noreply.github.com>
2023-11-23 18:24:08 -0300

    correcting errors
    correcting .env and private keys. correcting methods as well

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const MyPrivateKey = process.env.PRIVATE_KEY;
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
@@ -10,11 +11,16 @@ app.post("/authenticate", async (req, res) => {
 	const { username } = req.body;
 
 	try {
-		const r = await axios.put("https://api.chatengine.io/users/", {
-			username: username,
-			secret: username,
-			first_name: username,
-		});
+		const r = await axios.put(
+			"https://api.chatengine.io/users/",
+			{
+				username: username,
+				secret: username,
+				first_name: username,
+			},
+			{ headers: { "private-key": { MyPrivateKey } } }
+		);
+		return res.status(r.status).json(r.data);
 	} catch (error) {}
 
 	return res.json({ username: username, secret: "sha256..." });
