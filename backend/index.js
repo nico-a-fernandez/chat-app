03f7055df32e52d21614dc5d0642b386a17918c7
nico-a-fernandez <117313555+nico-a-fernandez@users.noreply.github.com>
2023-11-30 12:37:36 -0300

    correcting CORS
    correcting CORS

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(
 	cors({
 		origin: [
-			"http://localhost:3000",
+			"http://localhost:3001",
 			"https://chat-app-sigma-puce.vercel.app/",
 		],
 		credentials: true,
